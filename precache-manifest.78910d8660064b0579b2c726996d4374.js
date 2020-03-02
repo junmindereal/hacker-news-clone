28d9b50cd835320f604e6c7189ada4280d76ae8b
junmindereal <junmindereal@gmail.com>
2020-03-02 21:08:34 +0800

    Updates

---
+++ precache-manifest.78910d8660064b0579b2c726996d4374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4134d29180a4516d22d316a5ceb4abb9",
+    "url": "/hacker-news-clone/index.html"
+  },
+  {
+    "revision": "0306bee130ddb7175bbb",
+    "url": "/hacker-news-clone/static/css/main.cf639a9e.chunk.css"
+  },
+  {
+    "revision": "bc4295c6c3660c57e996",
+    "url": "/hacker-news-clone/static/js/2.ed667e45.chunk.js"
+  },
+  {
+    "revision": "f13e40ac39f80c649014b16312ac91b0",
+    "url": "/hacker-news-clone/static/js/2.ed667e45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0306bee130ddb7175bbb",
+    "url": "/hacker-news-clone/static/js/main.9ffd002e.chunk.js"
+  },
+  {
+    "revision": "34f9ce9e2a08881541d6",
+    "url": "/hacker-news-clone/static/js/runtime-main.020b449a.js"
+  }
+]);
